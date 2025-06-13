21f396aa5e66efba2b9ad434ac1e5173784abe8a
claireess <claricecostam@gmail.com>
2025-06-13 19:03:01 -0300

    Create activity1.py
    primeira atividade do curso da Studio Games.

---
+++ activity1.py
@@ -0,0 +1,18 @@
+ya = "euSeiFazerPrintAssimEAssim:"
+yaa = " helloWorld"
+
+
+print ya + " helloWorld!"
+print (yaa + " helloWorld!")
+
+
+a = "27"
+b = "11"
+
+
+print "ya = " + a + "yaa = " + b
+
+
+nice_division = 27 % 11
+print "IAMBATMAN"
+print (nice_division)
