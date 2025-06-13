f5af8a49839a28130574a24340b8569169e125b2
claireess <claricecostam@gmail.com>
2025-06-13 19:06:38 -0300

    Create activity4if.py
    quarta atividade do curso da Studio Games e segunda atividade do if.

---
+++ activity4if.py
@@ -0,0 +1,12 @@
+monster_life = 50
+print 'orc: i have ' + str(monster_life) + ' hearts, you wont be able to kill me'
+arrow = 8
+print 'hero: and with my ' + str(arrow) + ' arrows i\'ll defeat you'
+
+
+if arrow < monster_life :
+  print 'you lose'
+if arrow > monster_life :
+  print 'you win'
+if arrow == monster_life :
+  print 'you win'
