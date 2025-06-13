0526f1146e366560e54987ebf8b16a459388c4a8
claireess <claricecostam@gmail.com>
2025-06-13 19:08:49 -0300

    Create activity1else.py
    quinta atividade do curso da Studio Games e primeira vez usando o else.

---
+++ activity1else.py
@@ -0,0 +1,9 @@
+nublado = True
+chovendo = False
+if chovendo == True :
+  print 'levar o guarda-chuva'
+else :
+  if nublado == True :
+    print 'levar o guarda-chuva ainda assim'
+  else :
+    print 'nao precisa levar o guarda-chuva'
