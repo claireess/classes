2d10c78994379fcdc1dd49139163b00608dc4e6a
claireess <claricecostam@gmail.com>
2025-06-13 19:04:24 -0300

    Create activity2.py
    segunda atividade do curso da Studio Games.

---
+++ activity2.py
@@ -0,0 +1,16 @@
+age = 16
+my_age = "minha idade "
+#print my_age + str(age)
+
+
+height = "1.63"
+age += float(height)
+
+
+#print "Mais minha altura " + height + " resulta em " + str(int(age)) + " transformando em inteiro."
+
+
+height = True
+age = False
+print str(height) + " is True and " + str(age) + " is False."
+#height is true and age is true
